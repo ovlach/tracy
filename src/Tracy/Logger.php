62b41435e3575fa27abfd506cfd5cfdf3bcccdd1
Ondrej Hatala <ondrej.hatala@nubium.cz>
2015-06-25 14:55:21 +0200

    Debugger: directory for logger is not necesary
    Conflicts:
    	src/Tracy/Debugger.php
    	src/Tracy/Logger.php

--- src/Tracy/Logger.php
+++ src/Tracy/Logger.php
@@ -53,9 +53,11 @@ class Logger implements ILogger
 	 */
 	public function log($message, $priority = self::INFO)
 	{
-		if (!$this->directory) {
-			throw new \LogicException('Directory is not specified.');
-		} elseif (!is_dir($this->directory)) {
+		if (empty($this->directory))
+		{
+			return NULL;
+		}
+		if (!is_dir($this->directory)) {
 			throw new \RuntimeException("Directory '$this->directory' is not found or is not directory.");
 		}
 
